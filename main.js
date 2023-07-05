25cef85061b4c955a09d2000a1e46b0b2838c61b
diogo-ms <diogomarquessimoes@gmail.com>
2023-07-05 23:40:29 +0200

    button seems good, allows the user to ask for grid size

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
+const buttonGridSize = document.createElement('button');
+document.body.appendChild(buttonGridSize);
+buttonGridSize.id = 'button-grid-size';
+buttonGridSize.textContent = 'Grid Size'
 const container = document.createElement('div');
-container.id = 'container'
+container.id = 'container';
 document.body.appendChild(container);
 
 let pixels = document.querySelectorAll('.pixel');
 
 function createGrids(num) {
-  for (let i = 0; i <= num; i++) {
+  while (container.hasChildNodes()) {
+    container.removeChild(container.lastChild);
+  }
+  for (let i = 1; i <= num; i++) {    
     let row = document.createElement('div');
     row.className = 'row';
-    for (let j = 0; j <= num; j++) {
+    for (let j = 1; j <= num; j++) {
       let pixel = document.createElement('div')
       pixel.className = 'pixel';
       row.appendChild(pixel);
@@ -21,4 +28,12 @@ function brush(event) {
   pixel.style.backgroundColor = "black";
 }
 createGrids(16);
-container.addEventListener("mouseover", brush);
+container.addEventListener('mouseover', brush);
+buttonGridSize.addEventListener('click', () => {
+  let userGridSize = prompt('Ask your grid size (limit to 100): ');
+  if (userGridSize >= 100) {
+    alert("Error : Grid Size was set over the limit.")
+  } else {
+    createGrids(userGridSize)
+  }
+});
